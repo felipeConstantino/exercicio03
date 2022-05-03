8341d3313facb1f314c37cf012ed2f260bc6978d
felipeConstantino <91219970+felipeConstantino@users.noreply.github.com>
2022-05-03 17:35:42 -0300

    Update DAO.java

--- src/main/java/dao/DAO.java
+++ src/main/java/dao/DAO.java
@@ -16,18 +16,18 @@ public class DAO {
 		int porta = 5432;
 		String url = "jdbc:postgresql://" + serverName + ":" + porta +"/" + mydatabase;
 		String username = "ti2cc";
-		String password = "marcus_prado";
+		String password = "ti@cc";
 		boolean status = false;
 
 		try {
 			Class.forName(driverName);
 			conexao = DriverManager.getConnection(url, username, password);
 			status = (conexao == null);
-			System.out.println("Conex„o efetuada com o postgres!");
+			System.out.println("Conex√£o efetuada com o postgres!");
 		} catch (ClassNotFoundException e) { 
-			System.err.println("Conex„o N√O efetuada com o postgres -- Driver n„o encontrado -- " + e.getMessage());
+			System.err.println("Conex√£o N√ÉO efetuada com o postgres -- Driver n√£o encontrado -- " + e.getMessage());
 		} catch (SQLException e) {
-			System.err.println("Conex„o N√O efetuada com o postgres -- " + e.getMessage());
+			System.err.println("Conex√£o N√ÉO efetuada com o postgres -- " + e.getMessage());
 		}
 
 		return status;
